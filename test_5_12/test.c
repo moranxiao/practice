13928136d0e8a35cf684c9476d73409a2b5a70ae
moranxiao <yyyjka@foxmail.com>
2022-05-12 13:44:21 +0800

    日常打卡

--- test_5_12/test.c
+++ test_5_12/test.c
@@ -1,27 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-    char arr[10001] = { 0 };
-    int i = 0;
-    while (scanf("%c", &arr[i]) != EOF)
-    {
-        if (arr[i] == '\n')
-            arr[i] = '\0';
-        i++;
-    }
-    int right = strlen(arr) - 1;
-    int left = 0;
-    char t = 0;
-    while (left < right)
-    {
-        t = arr[left];
-        arr[left] = arr[right];
-        arr[right] = t;
-        left++;
-        right--;
-    }
-    printf("%s", arr);
-    return 0;
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int i = 0;
+	for (i = 1; (a * i) % b != 0; i++);
+	printf("%d", a * i);
+	return 0;
 }
