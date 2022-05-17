90757d78eb325f08ab6bbcffcea39af03203f0db
moranxiao <yyyjka@foxmail.com>
2022-05-17 23:48:01 +0800

    日常打卡

---
+++ test_5_17/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	for (int i = 0; i < 11; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	
+	return 0;
+}
