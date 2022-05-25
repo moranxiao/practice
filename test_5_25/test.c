a7e273f054bf1df9412a847fe647a1015e599e54
moranxiao <yyyjka@foxmail.com>
2022-05-25 21:02:29 +0800

    日常打卡

---
+++ test_5_25/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int j = 0;
+//    for (i = 1; i <= n; i++)
+//    {
+//        for (j = 1; j <= n - i; j++)
+//        {
+//            printf("  ");
+//        }
+//        for (; j <= n; j++)
+//            printf("* ");
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int  m, d, a;
+//    double pay;
+//    scanf("%lf %d %d %d", &pay, &m, &d, &a);
+//    if (m == 11 || d == 11)
+//    {
+//        if (a == 1)
+//        {
+//            pay *= 0.7;
+//            pay -= 50;
+//        }
+//        else
+//            pay *= 0.7;
+//    }
+//    else if (m == 12 || d == 12)
+//    {
+//        if (a == 1)
+//        {
+//            pay *= 0.8;
+//            pay -= 50;
+//        }
+//        else
+//            pay *= 0.8;
+//    }
+//    if (pay<=0)
+//        pay = 0.0;
+//    printf("%.2lf", pay);
+//
+//    return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int a, b, c, d;
+
+	return 0;
+}
