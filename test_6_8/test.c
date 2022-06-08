a60d946ba90584f437c5bc42f5f3e1e6f76b268f
墨染 <yyyjka@foxmail.com>
2022-06-08 18:48:59 +0800

    日常打卡

---
+++ test_6_8/test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    int arr1[100] = { 0 };
+    int arr2[100] = { 0 };
+    // ‰»Î
+    scanf("%d %d", &n, &m);
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+    //¥¶¿Ì
+    int j = 0;
+    i = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            printf("%d ", arr1[i]);
+            i++;
+        }
+        else
+        {
+            printf("%d ", arr2[j]);
+            j++;
+        }
+    }
+    if (i == n)
+    {
+        for (; j < m; j++)
+        {
+            printf("%d ", arr2[j]);
+        }
+    }
+    else
+    {
+        for (; i < n; i++)
+        {
+            printf("%d ", arr1[i]);
+        }
+    }
+    return 0;
+}
