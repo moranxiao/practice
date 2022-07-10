5b991ec8d55c1affba1b11202e7bdd03d48ccc16
墨染 <yyyjka@foxmail.com>
2022-07-10 18:37:34 +0800

    坚持学习

---
+++ test_7_10/test.c
@@ -0,0 +1,22 @@
+#pragma warning(disable:4996)
+//#pragma pack(4)
+#include <stdio.h>
+struct s1
+{
+	char a;
+	char b;
+	int c;
+};
+struct s
+{
+	int a;
+	char  b[10];
+	char c;
+	char d;
+	char e;
+};
+int main()
+{
+	printf("%zu", sizeof(struct s));
+	return 0;
+}
